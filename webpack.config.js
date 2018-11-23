b4b8f95563dbb2b61302ed5c03916a2d485753d7
ricokahler <ricokahler@justsift.com>
2018-11-23 16:53:23 -0500

    development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,33 @@
+const path = require('path');
+const resolveToUndefined = path.resolve(__dirname, './src/undefinedPlaceholder');
+
 module.exports = {
-  entry: './src/index.tsx',
-  output: './dist/'
-}
+  mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
+  entry: path.resolve(__dirname, './src/web.tsx'),
+  output: {
+    path: path.resolve('.'),
+    filename: 'sql-md.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: 'awesome-typescript-loader' }],
+      },
+      {
+        test: /\.css/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader', options: { minimize: true } }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.css'],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    alias: {
+      fs: resolveToUndefined,
+    },
+  },
+  externals: {
+    sqlMarkdown: 'sqlMarkdown',
+  },
+};
