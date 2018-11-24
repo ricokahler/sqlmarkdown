120da79892de9907daf6905b7e557193379d19ca
ricokahler <ricokahler@justsift.com>
2018-11-23 19:10:47 -0500

    working history with repl

--- src/components/Repl/Repl.tsx
+++ src/components/Repl/Repl.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import * as styles from 'styles';
+const { max, min } = Math;
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -29,11 +30,15 @@ const Body = styled.div`
   display: flex;
   flex-direction: column;
   padding: ${styles.space(-2)};
+  overflow: auto;
 `;
 const History = styled.div`
   flex: 1 1 auto;
 `;
-const Query = styled.div``;
+const Query = styled.div`
+  font-family: monospace;
+  margin-bottom: ${styles.space(-1)};
+`;
 const ButtonBar = styled.div`
   display: flex;
   align-items: center;
@@ -66,11 +71,13 @@ export interface ReplProps {
   onQuery: (query: string) => void;
   onExpandTerminal: () => void;
   onExpandAside: () => void;
+  onClear: () => void;
 }
 
 interface ReplState {
   hidden: boolean;
   query: string;
+  historyIndex: number | undefined;
 }
 
 export default class Repl extends React.PureComponent<ReplProps, ReplState> {
@@ -78,12 +85,72 @@ export default class Repl extends React.PureComponent<ReplProps, ReplState> {
   state: ReplState = {
     hidden: false,
     query: '',
+    historyIndex: undefined,
   };
 
   handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.currentTarget.value;
     const query = value.substring(prompt.length, value.length);
     this.setState({ query });
+  };
+
+  handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const { onQuery, queryHistory } = this.props;
+    const { query } = this.state;
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (!query) return;
+      onQuery(query);
+      this.setState({ query: '', historyIndex: undefined });
+      return;
+    }
+
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      this.setState(({ historyIndex }) => {
+        if (typeof historyIndex === 'number') {
+          const nextQuery = queryHistory[historyIndex - 1];
+          if (!nextQuery) return null;
+
+          return {
+            historyIndex: max(0, historyIndex - 1),
+            query: nextQuery,
+          };
+        }
+
+        const nextQuery = queryHistory[queryHistory.length - 1];
+        if (!nextQuery) return null;
+
+        return {
+          historyIndex: max(0, queryHistory.length - 1),
+          query: nextQuery,
+        };
+      });
+    }
+
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      this.setState(({ historyIndex }) => {
+        if (typeof historyIndex === 'number') {
+          const nextQuery = queryHistory[historyIndex + 1];
+          if (!nextQuery) return null;
+
+          return {
+            historyIndex: min(historyIndex + 1, queryHistory.length - 1),
+            query: nextQuery,
+          };
+        }
+
+        const nextQuery = queryHistory[0];
+        if (!nextQuery) return null;
+
+        return {
+          historyIndex: min(queryHistory.length + 1, queryHistory.length - 1),
+          query: nextQuery,
+        };
+      });
+    }
   };
 
   handleBodyClick = () => {
@@ -99,6 +166,7 @@ export default class Repl extends React.PureComponent<ReplProps, ReplState> {
       asideExpanded,
       onExpandTerminal,
       onExpandAside,
+      onClear,
     } = this.props;
     const { query } = this.state;
 
@@ -111,7 +179,7 @@ export default class Repl extends React.PureComponent<ReplProps, ReplState> {
           <IconButton onClick={onExpandTerminal}>
             <FontAwesomeIcon icon={terminalExpanded ? 'chevron-down' : 'chevron-up'} />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={onClear}>
             <FontAwesomeIcon icon="ban" />
           </IconButton>
         </ButtonBar>
@@ -121,7 +189,12 @@ export default class Repl extends React.PureComponent<ReplProps, ReplState> {
               <Query key={index}>{query}</Query>
             ))}
           </History>
-          <Input ref={this.inputRef} value={`${prompt}${query}`} onChange={this.handleInput} />
+          <Input
+            ref={this.inputRef}
+            value={`${prompt}${query}`}
+            onChange={this.handleInput}
+            onKeyDown={this.handleInputKeyDown}
+          />
         </Body>
       </Root>
     );
--- src/components/Repl/index.ts
+++ src/components/Repl/index.ts
@@ -1,22 +1,29 @@
 import Store from 'store';
-
 import Repl from './Repl';
 
 export default Store.withStore({
   mapStateToProps: state => {
     return {
-      queryHistory: ['test'],
+      queryHistory: state.queryHistory,
       terminalExpanded: state.terminalExpanded,
       asideExpanded: state.asideExpanded,
     };
   },
   mapDispatchToProps: dispatch => ({
-    onQuery: (query: string) => {},
+    onQuery: (query: string) => {
+      dispatch(state => ({
+        ...state,
+        queryHistory: [...state.queryHistory, query],
+      }));
+    },
     onExpandTerminal: () => {
       dispatch(state => ({ ...state, terminalExpanded: !state.terminalExpanded }));
     },
     onExpandAside: () => {
       dispatch(state => ({ ...state, asideExpanded: !state.asideExpanded }));
+    },
+    onClear: () => {
+      dispatch(state => ({ ...state, queryHistory: [] }));
     },
   }),
 })(Repl);
